0adabf4dcd36d6d94e327ddb0f7dd4bb4adecd7b
Дмитрий <osdal67@gmail.com>
2024-05-11 15:24:59 +0300

    123

--- resources/views/components/pop-categories.blade.php
+++ resources/views/components/pop-categories.blade.php
@@ -1,13 +1,31 @@
-<div class="pop-categories">Популярные категории</div>
-<div class="flex-1">
-    <img class="w-full" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-</div>
-<div class="flex-1">
-    <img class="w-full" src="{{ asset('img/top-banner-2.jpg') }}" alt="Пример изображения">
-</div>
-<div class="flex-1">
-    <img class="w-full" src="{{ asset('img/top-banner-3.jpg') }}" alt="Пример изображения">
-</div>
-<div class="flex-1">
-    <img class="w-full" src="{{ asset('img/top-banner-4.jpg') }}" alt="Пример изображения">
+<div class="font-w-bold">Популярные категории</div>
+
+<div class="flex gap-2 justify-center align-middle">
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-2.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-3.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-4.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-5.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-6.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-7.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-8.png') }}" alt="Пример изображения">
+    </div>
+    <div class="flex-1">
+        <img class="w-full" src="{{ asset('img/pop-categories/cat-9.png') }}" alt="Пример изображения">
+    </div>
 </div>
