cf869281e6836ac262b57d8367933c2a66e51562
Дмитрий <osdal67@gmail.com>
2024-05-13 13:07:05 +0300

    111

--- resources/views/components/pop-categories.blade.php
+++ resources/views/components/pop-categories.blade.php
@@ -1,7 +1,10 @@
 <div class="font-bold">Популярные категории</div>
 
 <div class="flex gap-2 justify-center align-middle">
-    <div class="flex-1 flex flex-col">
+    <div class="flex-1 flex flex-col relative">
+        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+            <p class="font-medium w-auto">до -15%</p>
+        </div>
         <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
         <p class="leading-6 text-center text-am">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
