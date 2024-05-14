524c42d0721078a01e931d633f2d01746a20d18d
Дмитрий <osdal67@gmail.com>
2024-05-14 11:35:55 +0300

    26262

--- resources/views/components/pop-categories.blade.php
+++ resources/views/components/pop-categories.blade.php
@@ -2,86 +2,85 @@
 
 <div class="flex gap-4 justify-between">
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-sm">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-2.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-3.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-4.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-5.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-6.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-7.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-8.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
     <div class="w-1/12 flex flex-col relative">
-        <div class="discount h-auto flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
+        <div class="discount h-auto w-4/5 flex justify-center align-middle absolute left-100 uppercase text-white bg-red-700 w-1/2 rounded-sm ">
             <p class="font-medium w-auto">до -15%</p>
         </div>
-        <img class="w-full pb-4" src="{{ asset('img/pop-categories/cat-1.png') }}" alt="Пример изображения">
-        <p class="leading-6 text-center text-am">
+        <img class="w-full pb-4 h-3/4" src="{{ asset('img/pop-categories/cat-9.png') }}" alt="Пример изображения">
+        <p class="leading-3 text-center text-xs font-medium text-blue-700">
             <a href="#" class="hover:text-red-500  focus:text-red-500 transition-colors duration-300">Профиль для гипсокартона</a>
         </p>
     </div>
-
     
 </div>
