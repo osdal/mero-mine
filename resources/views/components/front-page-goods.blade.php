0fc85a9998e3faa008752d8b272ae4f6965802aa
Дмитрий <osdal67@gmail.com>
2024-05-15 11:22:29 +0300

    klpo[

--- resources/views/components/front-page-goods.blade.php
+++ resources/views/components/front-page-goods.blade.php
@@ -1,3 +1,7 @@
-<div class="w-full grid w-full h-5 bg-slate-900">
-
+<div class="w-full grid grid-cols-5 gap-4 w-full h-5">
+    <div class="bg-slate-900"></div>
+    <div class="bg-slate-500"></div>
+    <div class="bg-slate-900"></div>
+    <div class="bg-slate-900"></div>
+    <div class="bg-slate-300"></div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,21 @@
     <x-layout-public>
         <x-banner-top />
         <x-Menu-top />
-        <div class="container mx-auto flex">
-            <div class="categories p-3 pl-0 gap-3 w-1/6">
-                <x-Categories />
-            </div>
-            <div class="content p-3 flex flex-col gap-4 w-full">
-                <x-Slider />
-                <div class="flex max-w-full gap-4">
-                    <x-Top-block-banners />
+        <div class="container mx-auto flex flex-col">
+            <div class="flex">
+                <div class="categories p-3 pl-0 gap-3 w-1/6">
+                    <x-Categories />
                 </div>
-                <x-Pop-categories />
-                <x-Front-page-goods />
+                <div class="content p-3 flex flex-col gap-4 w-full">
+                    <x-Slider />
+                    <div class="flex max-w-full gap-4">
+                        <x-Top-block-banners />
+                    </div>
+                    <x-Pop-categories />
+                </div>
             </div>
+                <x-Front-page-goods />
         </div>
-    </x-layout-public>
+</div>
+</x-layout-public>
 </div>
