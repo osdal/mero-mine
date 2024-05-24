36fca861aa2e4cf9f1bc51c4eb5fa2e4a232c745
Дмитрий <osdal67@gmail.com>
2024-05-24 19:10:33 +0300

    111

--- resources/views/components/front-page-goods.blade.php
+++ resources/views/components/front-page-goods.blade.php
@@ -97,14 +97,7 @@
                     </div>
                 </div>
             </div>
-            <div class="price mt-2 flex justify-between text-blue-900 font-semibold">
 
-            </div>
-
-            <div class="flex items-center gap-2">
-
-            </div>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
         </div>
     </div>
     <div class="card w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
