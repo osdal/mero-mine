38858f669efae331d66a038faa33a8a91ba6c1f5
Дмитрий <osdal67@gmail.com>
2024-05-27 14:16:58 +0300

    111

--- resources/views/components/front-page-goods.blade.php
+++ resources/views/components/front-page-goods.blade.php
@@ -50,7 +50,7 @@
             </a>
         </div>
         <div class="px-0 pb-5">
-            <div class="stars flex items-center mt-2.5 mb-2">
+            <div class="stars">
                 <div class="flex items-center space-x-1 rtl:space-x-reverse">
                     <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -71,7 +71,7 @@
                 <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
             </div>
             <a href="#" class="">
-                <h5 class="good-name text-sm font-bold tracking-tight text-blue-900 dark:text-white">Гипсокартон влагостойкий стеновой Knauf 2000х1200х12,5 мм</h5>
+                <h5 class="good-name">Гипсокартон влагостойкий стеновой Knauf 2000х1200х12,5 мм</h5>
             </a>
             <div class="data grid grid-cols-[auto,1fr] w-full gap-2">
                 <p class=" text-blue-900">Цена:</p>
