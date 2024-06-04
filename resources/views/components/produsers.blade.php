afcba9dbc7ff60d5fe128f6b92e76768804cc31a
Дмитрий <osdal67@gmail.com>
2024-06-04 20:37:42 +0300

    v

--- resources/views/components/produsers.blade.php
+++ resources/views/components/produsers.blade.php
@@ -1,25 +1,50 @@
-<div class="video-block flex flex-col ">
-    <div class="videos ">
-        <div>
-            <img class=" basis-0 flex-shrink" src="img/video-block/video-1.jpg" alt="">
-            <p>Вигоди стає вдвічі більше: Mepo та два українських банки анонсували випуск нового ко-продукту.
-            <br><br>23.11.2023</p>
-        </div>
-        <div>
-            <img class=" basis-0 flex-shrink" src="img/video-block/video-2.jpg" alt="">
-            <p>Вигоди стає вдвічі більше: Mepo та два українських банки анонсували випуск нового ко-продукту.
-            <br><br>23.11.2023</p>
-        </div>
-        <div>
-            <img class=" basis-0 flex-shrink" src="img/video-block/video-3.jpg" alt="">
-            <p>Вигоди стає вдвічі більше: Mepo та два українських банки анонсували випуск нового ко-продукту.
-            <br><br>23.11.2023</p>
-        </div>
-        <div>
-            <img class=" basis-0 flex-shrink" src="img/video-block/video-4.jpg" alt="">
-            <p>Вигоди стає вдвічі більше: Mepo та два українських банки анонсували випуск нового ко-продукту.
-            <br><br>23.11.2023</p>
-        </div>
-
-</div>
+<div class="produsers flex justify-between ">
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-1.jpg" alt="">
+        <p>Sniezka</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-2.jpg" alt="">
+        <p>Ceresit</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-3.jpg" alt="">
+        <p>Dufa</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-4.jpg" alt="">
+        <p>BAUMIT</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-5.jpg" alt="">
+        <p>Metylan</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-6.jpg" alt="">
+        <p>CAPAROL</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-7.jpg" alt="">
+        <p>KNAUF</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-8.jpg" alt="">
+        <p>TYTAN</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-1.jpg" alt="">
+        <p>CAPAROL</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-2.jpg" alt="">
+        <p>Sniezka</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-3.jpg" alt="">
+        <p>Ceresit</p>
+    </div>
+    <div>
+        <img class=" basis-0 flex-shrink" src="img/produsers-logos/logo-4.jpg" alt="">
+        <p>CAPAROL</p>
+    </div>
 </div>
