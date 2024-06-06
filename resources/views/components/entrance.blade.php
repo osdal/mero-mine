90ab399f841838f97b195911dea43eb2afdcb8b6
Дмитрий <osdal67@gmail.com>
2024-06-06 07:15:16 +0300

    cdws

--- resources/views/components/entrance.blade.php
+++ resources/views/components/entrance.blade.php
@@ -1,12 +1,15 @@
-<div class="entrance">
-    <h2 class="">Добро пожаловать!</h2>
-    <p>Войдите, чтобы получать рекомендации,
+<div class="entrance flex flex-col items-center gap-3">
+    <h2 class="font-bold">Добро пожаловать!</h2>
+    <p class="font-semibold text-xs text-center">Войдите, чтобы получать рекомендации,
         персональные бонусы и скидки.</p>
-        <a href="#">Войдите в личный кабинет</a>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Войдите в личный кабинет</button>
         <div class="social">
             <p>Мы в социальных сетях</p>
-            <div class="">
-                <img src="" alt="">
+            <div class="flex gap-5">
+                <img src="img/social/youtube.jpg" alt="">
+                <img src="img/social/insta.jpg" alt="">
+                <img src="img/social/viber.jpg" alt="">
+                <img src="img/social/telegram.jpg" alt="">
             </div>
         </div>
 </div>
